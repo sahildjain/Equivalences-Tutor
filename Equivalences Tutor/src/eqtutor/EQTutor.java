37ea074a54fa33295459eac78c7c4ee2ed95366f
Sahil Jain <sdj09@ic.ac.uk>
2013-01-29 16:37:54 +0000

    AST seems to be working. need to get around the nesting problem though. so add rules for nesting

--- Equivalences Tutor/src/eqtutor/EQTutor.java
+++ Equivalences Tutor/src/eqtutor/EQTutor.java
@@ -14,7 +14,8 @@ import AST.*;
 public class EQTutor {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		String src = "a -> b & c";
+		String src = "a <> b -> c & d | e <> f";
+		System.out.println("Formula: " + src);
 		CharStream stream = new ANTLRStringStream(src);
 		LogicLexer lexer = new LogicLexer(stream);
 		TokenStream tokens = new CommonTokenStream(lexer);
