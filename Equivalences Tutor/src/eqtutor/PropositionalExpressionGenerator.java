7df1685f16fd75f83a5b914e1b5ada17685aa95f
Sahil Jain <sdj09@ic.ac.uk>
2013-01-30 17:43:54 +0000

    added unaryoperator.java

--- Equivalences Tutor/src/eqtutor/PropositionalExpressionGenerator.java
+++ Equivalences Tutor/src/eqtutor/PropositionalExpressionGenerator.java
@@ -14,24 +14,69 @@ public class PropositionalExpressionGenerator extends ExpressionGenerator {
 	
 	public String generate() {
 		StringBuilder stringBuilder = new StringBuilder();
-		BinaryOperator operator;
-		int nestings = getNestings();
+		/*int nestings = getNestings();*/
 		int atoms = getAtoms();
-		for(int i = 0; i < atoms; i++) {
-			stringBuilder.append(getAtom(i));
-			operator = getRandomOperator();
-			if(i != atoms - 1) {
-				stringBuilder.append(getOperatorSymbol(operator));
+		Random randomAtoms = new Random();
+		int randAtoms;
+		for(int i = 0; i < atoms; i += randAtoms) {
+			randAtoms = randomAtoms.nextInt(atoms - i);
+			/*Random randomNestings = new Random();
+			int randNestings = randomNestings.nextInt(nestings);
+			if(randNestings > randAtoms) {
+				randNestings = randAtoms - 1;
 			}
+			nestings -= randNestings;*/
+			//boolean end = false;
+			//if(randAtoms == atoms) {
+			//	end = true;
+			//}
+			PropositionalExpressionGenerator gen = new PropositionalExpressionGenerator(randAtoms, 0);
+			stringBuilder.append(gen.generateExpression(i));
 		}
 		return stringBuilder.toString();
 	}
 	
+	private String generateString(int offset) {
+		int nestings = getNestings();
+		int atoms = getAtoms();
+		if(atoms == 1) {
+			return getAtom(offset);
+		}
+		StringBuilder stringBuilder = new StringBuilder();
+		int i;
+		for(i = 0; i < atoms; i++) {
+			BinaryOperator operator = getRandomOperator();
+			stringBuilder.append(getAtom(offset + i) + getOperatorSymbol(operator));
+		}
+		return stringBuilder.toString();
+	}
+	
+	private String generateExpression(int offset) {
+		StringBuilder stringBuilder = new StringBuilder();
+		int atoms = getAtoms();
+		int nestings = getNestings();
+		if(nestings > 0) {
+			stringBuilder.append("(");
+		}
+		for(int i = 0; i < atoms; i ++) {
+			stringBuilder.append(getAtom(offset + i));
+			BinaryOperator operator = getRandomOperator();
+			if(i != atoms - 1) {
+				stringBuilder.append(getOperatorSymbol(operator));
+			}
+		}
+		if(nestings > 0) {
+			stringBuilder.append(")");
+			nestings--;
+		}
+		return stringBuilder.toString();
+	}
+
 	private String getAtom(int offset) {
 		int ascii = 97 + offset;
 		Random random = new Random();
 		int rand = random.nextInt(10);
-		if(rand > 7) {
+		if(rand >= 7) {
 			return "!" + Character.toString((char) ascii);
 		}
 		return Character.toString((char) ascii);
@@ -41,16 +86,8 @@ public class PropositionalExpressionGenerator extends ExpressionGenerator {
 		return atoms;
 	}
 
-	public void setAtoms(int atoms) {
-		this.atoms = atoms;
-	}
-
 	public int getNestings() {
 		return nestings;
-	}
-
-	public void setNestings(int nestings) {
-		this.nestings = nestings;
 	}
 	
 }
---
+++ Equivalences Tutor/src/eqtutor/UnaryOperator.java
@@ -0,0 +1,5 @@
+package eqtutor;
+
+public enum UnaryOperator {
+	NOT
+}
