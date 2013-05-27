23ea041087330d96f90f55cfcc1c0dc0e4336bbe
Sahil Jain <sdj09@ic.ac.uk>
2013-05-27 23:46:29 +0100

    identifier dialog

--- Equivalences Tutor/src/dialogs/IdentifierDialog.java
+++ Equivalences Tutor/src/dialogs/IdentifierDialog.java
@@ -1,7 +1,10 @@
 package dialogs;
 
+import java.awt.BorderLayout;
+
 import gui.NewPersonalEquivalenceListener;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,12 +23,16 @@ public class IdentifierDialog extends JDialog {
 		this.setListener(listener);
 		this.setFrame(listener.getFrame());
 		panel = new JPanel(new MigLayout());
-		//panel.add(addIdempotence(), BorderLayout.NORTH);
-		//panel.add(addCommutativity(), BorderLayout.NORTH);
+		panel.add(addDoubleNegation(), BorderLayout.NORTH);
 		getContentPane().add(panel);
 		pack();
 		setLocationRelativeTo(getFrame());
 		setVisible(true);
+	}
+	
+	private JButton addDoubleNegation() {
+		JButton button = new JButton("Double Negation: A = \u00AC\u00ACA");
+		return button;
 	}
 
 	public NewPersonalEquivalenceListener getListener() {
